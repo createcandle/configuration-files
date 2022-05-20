0ff717c104a19816da4fd70f58c204cc97b6f6a5
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-05-20 11:42:27 +0200

    Update prepare_for_disk_image.sh

--- home/pi/prepare_for_disk_image.sh
+++ home/pi/prepare_for_disk_image.sh
@@ -7,6 +7,7 @@ fi
 
 # prepare for factory reset script
 touch /boot/developer.txt # will cause the real factory_reset script to also write zeroes to empty space
+rm /home/pi/candle.log
 rm /boot/keep_z2m.txt
 rm /boot/keep_bluetooth.txt
 rm /boot/candle_first_run_complete.txt
