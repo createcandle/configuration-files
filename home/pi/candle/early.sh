7bd268d89f62b9e1bf1b25f00c1f037d4718509c
flatsiedatsie <unfold@gmail.com>
2022-08-01 17:25:49 +0200

    Update early.sh

--- home/pi/candle/early.sh
+++ home/pi/candle/early.sh
@@ -4,3 +4,14 @@ echo "in Candle early"
 # fix hostname
 /usr/bin/hostname -F /home/pi/.webthings/etc/hostname
 systemctl restart avahi-daemon.service
+
+if iptables --list | grep 4443; then
+    echo "IPTABLES ALREADY ADDED"
+else
+    iptables -t mangle -A PREROUTING -p tcp --dport 80 -j MARK --set-mark 1
+    iptables -t mangle -A PREROUTING -p tcp --dport 443 -j MARK --set-mark 1
+    iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+    iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4443
+    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -m mark --mark 1 -j ACCEPT
+    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -m mark --mark 1 -j ACCEPT
+fi
