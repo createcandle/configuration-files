8605970000a0f8583faaffc5e44c76cfe45d5fa6
flatsiedatsie <unfold@gmail.com>
2022-08-09 19:29:13 +0200

    Update early.sh

--- home/pi/candle/early.sh
+++ home/pi/candle/early.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "in Candle early"
+echo "in Candle early" >> /dev/kmsg
 
 # fix hostname
 /usr/bin/hostname -F /home/pi/.webthings/etc/hostname
 systemctl restart avahi-daemon.service
+
+rm /boot/candle_rw_once.txt
 
 if iptables --list | grep 4443; then
     echo "IPTABLES ALREADY ADDED"
@@ -15,3 +18,5 @@ else
     iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -m mark --mark 1 -j ACCEPT
     iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -m mark --mark 1 -j ACCEPT
 fi
+
+
