df27a069ef95746b5959320617b558e475e1690b
flatsiedatsie <unfold@gmail.com>
2022-08-11 12:41:38 +0200

    Update early.sh

--- home/pi/candle/early.sh
+++ home/pi/candle/early.sh
@@ -1,40 +1,18 @@
 #!/bin/bash
+set +e
 
 # This runs even before rc.local
 
 echo "in Candle early"
 echo "in Candle early. Fixing hostname." >> /dev/kmsg
 
-# fix hostname
+
+# Fix hostname
 /usr/bin/hostname -F /home/pi/.webthings/etc/hostname
 systemctl restart avahi-daemon.service
 
-# Handled forced restore of rc.local
-if [ -f /boot/restore_boot_backup.txt ];
-then
-  rm /boot/restore_boot_backup.txt
-  if [ -f /etc/rc.local.bak ];
-  then
-    cp /etc/rc.local.bak /etc/rc.local
-  else
-    touch /boot/restore_boot_backup_failed.txt
-  fi
-fi
 
-# Handled forced untar of Candle Controller
-if [ -f /boot/restore_controller_backup.txt ];
-then
-  rm /boot/restore_controller_backup.txt
-  if [ -f /home/pi/controller_backup.tar ];
-  then
-    rm -rf /home/pi/webthings
-  else
-    touch /boot/restore_controller_backup_failed.txt
-  fi
-fi
-
-#rm /boot/candle_rw_once.txt
-
+# Add firewall rules
 if iptables --list | grep 4443; then
     echo "IPTABLES ALREADY ADDED"
 else
@@ -46,5 +24,62 @@ else
     iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -m mark --mark 1 -j ACCEPT
     iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -m mark --mark 1 -j ACCEPT
 fi
+
+
+# Handle forced restore of rc.local
+if [ -f /boot/restore_boot_backup.txt ];
+then
+  rm /boot/restore_boot_backup.txt
+  if [ -f /etc/rc.local.bak ];
+  then
+    cp /etc/rc.local.bak /etc/rc.local
+    echo "$(date) - forced restored boot backup" >> /boot/candle.log
+    echo "$(date) - forced restored boot backup" >> /dev/kmsg
+
+  else
+    echo "$(date) - forced restoring boot backup: no backup found" >> /boot/candle.log
+    echo "$(date) - forced restoring boot backup: no backup found" >> /dev/kmsg
+  fi
+fi
+
+
+# Handle forced restore of Candle Controller backup
+if [ -f /boot/restore_controller_backup.txt ];
+then
+  rm /boot/restore_controller_backup.txt
+  if [ -f /home/pi/controller_backup.tar ];
+  then
+    cd /home/pi || exit
+    echo "$(date) - forced restoring controller backup..." >> /boot/candle.log
+    echo "$(date) - forced restoring controller backup..." >> /dev/kmsg
+    rm -rf /home/pi/webthings
+    tar -xvf controller_backup.tar
+    echo "$(date) - forced restoring controller backup done" >> /boot/candle.log
+    echo "$(date) - forced restoring controller backup done" >> /dev/kmsg
+  else
+    echo "$(date) - forced restoring controller backup: no backup found" >> /boot/candle.log
+    echo "$(date) - forced restoring controller backup: no backup found" >> /dev/kmsg
+  fi
+fi
+
+
+# If the user wants a forced rebuild, do that.
+elif [ -f /boot/force_controller_rebuild.txt ];
+then
+  rm /boot/force_controller_rebuild.txt
+  cd /home/pi || exit
+  echo "$(date) - starting forced controller regeneration..." >> /boot/candle.log
+  echo "$(date) - starting forced controller regeneration..." >> /dev/kmsg
+  wget https://raw.githubusercontent.com/createcandle/install-scripts/main/install_candle_controller.sh
+  chmod +x ./install_candle_controller.sh
+  sudo -u pi ./install_candle_controller.sh
+  rm ./install_candle_controller.sh
+  echo "$(date) - forced controller regeneration done" >> /boot/candle.log
+  echo "$(date) - forced controller regeneration done" >> /dev/kmsg
+fi
+
+
+
+
 
 
