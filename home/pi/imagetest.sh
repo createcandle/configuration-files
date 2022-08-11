1f688ab3c75bda9ebb93482d40cc320391e63ebe
flatsiedatsie <unfold@gmail.com>
2022-08-11 10:04:39 +0200

    Update imagetest.sh

--- home/pi/imagetest.sh
+++ home/pi/imagetest.sh
@@ -8,8 +8,10 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo "Hostname:"
+cat /etc/hostname
 
-
+echo
 echo "ERRORS:"
 
 if [ ! -L /dev/hosts ]; then
@@ -18,10 +20,29 @@ fi
 if [ ! -L /home/pi/.asoundrc ]; then
   echo "/home/pi/.asoundrc is not a link"
 fi
+
+if [ ! -d /etc/hostapd ]; then
+  echo "/etc/hostapd is missing"
+fi
+if [ ! -d /etc/hostapd ]; then
+  echo "/etc/hostapd is missing"
+fi
+
 if [ ! -f /home/pi/.webthings/floorplan.svg ]; then
   echo "/home/pi/.webthings/floorplan.svg is missing"
 fi
+if [ ! -f /home/pi/webthings/gateway/build/app.js ]; then
+  echo "/home/pi/webthings/gateway/build/app.js is missing"
+fi
+if [ ! -f /home/pi/webthings/gateway/.post_upgrade_complete ]; then
+  echo "/home/pi/webthings/gateway/.post_upgrade_complete is missing"
+fi
+if [ ! -f /home/pi/webthings/gateway/build/static/css/candle.css ]; then
+  echo "/home/pi/webthings/gateway/build/static/css/candle.css is missing"
+fi
 
+echo "Machine ID:"
+cat /etc/machine-id
 
 echo
 echo "---------------------------------------------"
@@ -30,6 +51,13 @@ systemctl list-units --failed
 
 echo
 journalctl --boot=0 --priority=0..3
+
+echo
+echo "---------------------------------------------"
+echo
+
+echo "/boot/config.txt:"
+cat /boot/config.txt | grep -v "#" | grep .
 
 
 echo
