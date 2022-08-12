9de158d887b58bd5cdbdeb8e07e54e2ac13093b6
flatsiedatsie <unfold@gmail.com>
2022-08-12 14:05:27 +0200

    Update debug.sh

--- home/pi/debug.sh
+++ home/pi/debug.sh
@@ -13,7 +13,7 @@ echo "Hostname       : $(cat /etc/hostname)"
 
 echo
 echo "--------------------------------------------- ERRORS"
-
+echo
 if [ ! -L /etc/hosts ]; then
   echo "/etc/hosts is not a link"
 fi
@@ -71,8 +71,10 @@ echo
 systemctl list-units --failed
 echo
 journalctl --boot=0 --priority=0..3
-echo "(NOTE: syslog.socket failing is expected, as Candle will only log to syslog if developer.txt is present")
-echo "(NOTE: This error is also not a problem: Failed to make bind mount source '/home/pi/.webthings/etc/hostname': File exists)"
+echo
+echo "NOTE:"
+echo "- syslog.socket failing is expected, as Candle will only log to syslog if developer.txt is present"
+echo "- This error is also not a problem: Failed to make bind mount source '/home/pi/.webthings/etc/hostname': File exists"
 
 echo
 echo "--------------------------------------------- dmesg"
@@ -85,7 +87,9 @@ echo
 echo "dmesg warnings:"
 dmesg --level=warn
 
-
+echo "NOTE:"
+echo "- Some warnings are to be expected, and they are not really indicative of a real problem"
+echo "- Not all of these are even warnings, as Candle's processes also report when they start and stop".
 
 echo
 echo "--------------------------------------------- config"
@@ -118,13 +122,19 @@ findmnt -t ext4
 echo
 df
 
+echo "NOTE:"
+echo "- /dev/mmcblk0p1 is the boot partition, which is what you see when you plug the SD card into your computer. Candle.log and debug.txt are safe to delete from it."
+echo "- /dev/mmcblk0p2 is the system partition. It this is (almost) full, that means an upgrade went very wrong."
+echo "- /dev/mmcblk0p3 is the user partition, where your actual personal data is stored. If it's (almost) full, delete some logs or photos?"
 echo
 echo "--------------------------------------------- memory"
 echo
 free -h
 echo "Allocated GPU memory: $(vcgencmd get_mem gpu)"
 
-
+echo "NOTE:"
+echo "- What matters is the memory under 'available'. It it's lower than 100, try uninstalling some addons."
+echo
 echo
 echo "--------------------------------------------- startup"
 echo
