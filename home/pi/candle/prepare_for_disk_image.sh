25f2efd029456f6b42a197468b488b11c7c62602
flatsiedatsie <unfold@gmail.com>
2022-08-15 01:43:59 +0200

    Create prepare_for_disk_image.sh

---
+++ home/pi/candle/prepare_for_disk_image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo"
+  exit
+fi
+
+# prepare for factory reset script
+touch /boot/developer.txt # will cause the real factory_reset script to also write zeroes to empty space
+rm /home/pi/.webthings/candle.log
+rm /boot/keep_z2m.txt
+rm /boot/keep_bluetooth.txt
+rm /boot/candle_first_run_complete.txt
+rm /boot/candle_rw_keep.txt
+rm /boot/candle_rw_once.txt
+rm /boot/tunnel.txt
+rm /etc/asound.conf
+rm /var/log/*
+cp /home/pi/candle/candle_first_run.sh /boot/candle_first_run.sh
+
+
+chmod +x /home/pi/.webthings/addons/power-settings/factory_reset.sh
+/home/pi/.webthings/addons/power-settings/factory_reset.sh
