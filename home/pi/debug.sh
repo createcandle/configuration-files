304079b81923e6bf6a3e6f6e6bd92cfe0370c2b9
flatsiedatsie <unfold@gmail.com>
2022-08-15 00:49:33 +0200

    Update debug.sh

--- home/pi/debug.sh
+++ home/pi/debug.sh
@@ -95,6 +95,9 @@ fi
 if [ ! -f /home/pi/webthings/gateway/.post_upgrade_complete ]; then
   echo "/home/pi/webthings/gateway/.post_upgrade_complete is missing"
 fi
+if [ ! -f /home/pi/.webthings/etc/webthings_settings_backup.js ]; then
+  echo "/home/pi/.webthings/etc/webthings_settings_backup.js is missing"
+fi
 if [ ! -f /home/pi/webthings/gateway/static/images/floorplan.svg ]; then
   echo "/home/pi/webthings/gateway/static/images/floorplan.svg is missing"
 fi
