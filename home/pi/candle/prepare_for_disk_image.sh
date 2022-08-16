fd029e00d1103a70da52efefc4b1a45fc9acd6e7
flatsiedatsie <unfold@gmail.com>
2022-08-16 17:21:25 +0200

    Update prepare_for_disk_image.sh

--- home/pi/candle/prepare_for_disk_image.sh
+++ home/pi/candle/prepare_for_disk_image.sh
@@ -9,6 +9,7 @@ fi
 touch /boot/developer.txt # will cause the real factory_reset script to also write zeroes to empty space
 rm /home/pi/.webthings/candle.log
 rm /boot/candle_log.txt
+rm /boot/candle_cutting_edge.txt
 rm /boot/keep_z2m.txt
 rm /boot/keep_bluetooth.txt
 rm /boot/candle_first_run_complete.txt
