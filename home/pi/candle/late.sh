6d0b6278c426909c92d75380ea5b561d6ccac920
flatsiedatsie <unfold@gmail.com>
2022-08-18 23:26:11 +0200

    Update late.sh

--- home/pi/candle/late.sh
+++ home/pi/candle/late.sh
@@ -6,5 +6,16 @@ echo "In late" >> /dev/kmsg
 
 # Do aditional checks for missing files, and restore them if possible.
 if [ -d /home/pi/candle/configuration-files-backup ]; then
-    rsync --ignore-existing -vr /home/pi/candle/configuration-files-backup/* /
+    if rsync --ignore-existing --dry-run -vri /home/pi/candle/configuration-files-backup/* / | grep -q  +++++; then
+        if [ -d /ro ]; then
+            sudo mount -o remount,ro /rw
+        fi
+        echo "Candle: late: fixing missing"
+        rsync --ignore-existing -vr /home/pi/candle/configuration-files-backup/* / >> /dev/kmsg
+        if [ -d /ro ]; then
+            sudo mount -o remount,ro /ro
+        fi
+    fi
+else
+    echo "configuration files backup dir does not exist"
 fi
