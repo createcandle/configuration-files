3df7d50da3fded2478de432bb23b97884d54d52a
flatsiedatsie <unfold@gmail.com>
2022-08-18 21:36:03 +0200

    Update late.sh

--- home/pi/candle/late.sh
+++ home/pi/candle/late.sh
@@ -4,4 +4,7 @@ set +e
 #/bin/echo "in late" | sudo tee -a /dev/kmsg
 echo "In late" >> /dev/kmsg
 
-
+# Do aditional checks for missing files, and restore them if possible.
+if [ -d /home/pi/candle/configuration-files-backup ]; then
+    rsync --ignore-existing -vr /home/pi/candle/configuration-files-backup/* /
+fi
