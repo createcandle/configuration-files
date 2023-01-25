4dbbfda287b12665181c81740811ffef1b1d9140
flatsiedatsie <unfold@gmail.com>
2023-01-25 18:31:54 +0100

    Update candle_first_run.sh

--- home/pi/candle/candle_first_run.sh
+++ home/pi/candle/candle_first_run.sh
@@ -69,6 +69,14 @@ fi
 echo "$(date) - candle_first_run.sh: new SSH security keys generated." >> /boot/candle_log.txt
 
 
+# Generate SSL keys
+WEBTHINGS_HOME="${WEBTHINGS_HOME:=${HOME}/.webthings}"
+SSL_DIR="${WEBTHINGS_HOME}/ssl"
+[ ! -d "${SSL_DIR}" ] && mkdir -p "${SSL_DIR}"
+openssl genrsa -out "${SSL_DIR}/privatekey.pem" 2048
+openssl req -new -sha256 -key "${SSL_DIR}/privatekey.pem" -out "${SSL_DIR}/csr.pem" -subj '/CN=www.sgnihtbew.com/O=Candle Controller/C=US'
+openssl x509 -req -in "${SSL_DIR}/csr.pem" -signkey "${SSL_DIR}/privatekey.pem" -out "${SSL_DIR}/certificate.pem"
+
 
 # If the disk image was created on Windows or Mac, it may leaves behind cruft
 rm -rf /boot/'System Volume Information'
