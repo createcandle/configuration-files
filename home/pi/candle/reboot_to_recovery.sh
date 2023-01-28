7eab1447e7f16bdd62b1185e4dc41aaa5f204701
flatsiedatsie <unfold@gmail.com>
2023-01-28 12:34:02 +0100

    Update reboot_to_recovery.sh

--- home/pi/candle/reboot_to_recovery.sh
+++ home/pi/candle/reboot_to_recovery.sh
@@ -8,7 +8,8 @@ echo "reboot to recovery activated by holding ALT key" >> /boot/candle_log.txt
 if [ -f /boot/cmdline-update.txt ] && [ -f /boot/cmdline-candle.txt ]; then
   cp /boot/cmdline-update.txt /boot/cmdline.txt
   touch /boot/candle_ssh_once.txt
-  reboot
+  timeout 2 cat /dev/urandom > /dev/fb0
+  reboot -f
 else
   echo "Could not reboot to recovery because cmdline-update.txt or cmdline-candle.txt was missing" >> /dev/kmsg
 fi
