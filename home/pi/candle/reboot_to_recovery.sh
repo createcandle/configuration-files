6974fd5f0b76bd02f27e764d0c9a5ebcb8697d09
flatsiedatsie <unfold@gmail.com>
2023-11-17 18:26:20 +0100

    Update reboot_to_recovery.sh

--- home/pi/candle/reboot_to_recovery.sh
+++ home/pi/candle/reboot_to_recovery.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+BOOT_DIR="/boot"
+if lsblk | grep $BOOT_DIR/firmware; then
+    echo "firmware partition is mounted at $BOOT_DIR/firmware"
+    BOOT_DIR="$BOOT_DIR/firmware"
+fi
+
 echo "in reboot to recovery"
 echo "reboot to recovery activated by holding ALT key" >> /dev/kmsg
-echo "reboot to recovery activated by holding ALT key" >> /boot/candle_log.txt
+echo "reboot to recovery activated by holding ALT key" >> $BOOT_DIR/candle_log.txt
 
 # Make sure both command line files exist so we can also switch back once in recovery
-if [ -f /boot/cmdline-update.txt ] && [ -f /boot/cmdline-candle.txt ]; then
-  cp /boot/cmdline-update.txt /boot/cmdline.txt
-  touch /boot/candle_ssh_once.txt
+if [ -f $BOOT_DIR/cmdline-update.txt ] && [ -f $BOOT_DIR/cmdline-candle.txt ]; then
+  cp $BOOT_DIR/cmdline-update.txt $BOOT_DIR/cmdline.txt
+  touch $BOOT_DIR/candle_ssh_once.txt
   if [ -e /dev/fb0 ]; then
     timeout 2 cat /dev/urandom > /dev/fb0
   fi
