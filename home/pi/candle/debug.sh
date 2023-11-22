bc8700fa93cf9068fe98010ac479885c41e38acb
flatsiedatsie <unfold@gmail.com>
2023-11-22 18:17:39 +0100

    Update debug.sh

--- home/pi/candle/debug.sh
+++ home/pi/candle/debug.sh
@@ -10,8 +10,8 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 BOOT_DIR="/boot"
-if lsblk | grep $BOOT_DIR/firmware; then
-    echo "firmware partition is mounted at $BOOT_DIR/firmware"
+if lsblk | grep -q $BOOT_DIR/firmware; then
+    #echo "firmware partition is mounted at $BOOT_DIR/firmware"
     BOOT_DIR="$BOOT_DIR/firmware"
 fi
 
