42992f91d12487bb70d2cbca70288386e665599c
flatsiedatsie <unfold@gmail.com>
2023-12-06 22:49:28 +0100

    Update kiosk.sh

--- home/pi/candle/kiosk.sh
+++ home/pi/candle/kiosk.sh
@@ -27,30 +27,23 @@ then
   if [ "$totalk" -gt 1500000 ] || [ -f $BOOT_DIR/candle_kiosk_forced.txt ]
   then
     echo "Candle: Enough memory to start X server" >> /dev/kmsg
-    #mkdir -p /tmp/xdg
- 
-    #export XDG_RUNTIME_DIR="/tmp/xdg"
-    mkdir -p /run/user/1000
-
-    /usr/bin/X -nolisten tcp &
-    sleep 4
-
+    logger Starting X
     if [ -n "$(ls /dev/input/by-id/usb-ILITEK_ILITEK-TP-mouse 2>/dev/null)" ]
     then
       # Raspad touchscreen
-      startx -- -nocursor
+      su - pi -c 'startx -- -nocursor &'
     else
       # any mouse
       if [ -n "$(ls /dev/input/by-id/*-mouse 2>/dev/null)" ]
       then
-        startx
+        su - pi -c startx &
         #su - pi -c 'unclutter -idle 10 -root -display :0 &'
       else
         if [ ! -f $BOOT_DIR/hide_mouse_pointer.txt ]
         then
-          startx
+          su - pi    startx &
         else
-          startx -- -nocursor
+          su - pi -c 'startx -- -nocursor &'
           #su - pi -c 'unclutter -idle 0 -root -display :0 &'
         fi
       fi
