2e8152a06cd2040fd83f5d37fb204b8c82e19d63
flatsiedatsie <unfold@gmail.com>
2023-12-23 22:55:22 +0100

    Update kiosk.sh

--- home/pi/candle/kiosk.sh
+++ home/pi/candle/kiosk.sh
@@ -8,29 +8,47 @@ if lsblk | grep -q $BOOT_DIR/firmware; then
 fi
 
 
-if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f "/boot/splash.png" ] && [ -f "/boot/splash180.png" ]; then
-    if [ -e "/boot/rotate180.txt" ]; then
-        /bin/ply-image /boot/splash180.png
+if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f "$BOOT_DIR/splash.png" ] && [ -f "$BOOT_DIR/splash180.png" ]; then
+    if [ -e "$BOOT_DIR/rotate180.txt" ]; then
+        /bin/ply-image $BOOT_DIR/splash180.png
     else
-        /bin/ply-image /boot/splash.png
+        /bin/ply-image $BOOT_DIR/splash.png
     fi
-    sleep 1
+    #sleep 1
 fi
 
-sleep 5
+#if [ -f $BOOT_DIR/post_bootup_actions.sh ]; then
+#  if [ -f $BOOT_DIR/rotate180.txt ]; then
+#    feh --bg-fill $BOOT_DIR/splash_updating180.png &
+#  else
+#    feh --bg-fill $BOOT_DIR/splash_updating.png &
+#  fi
+#fi
+
+timeout --foreground -s TERM 30s bash -c \
+   'while [[ "$(curl -s -o /dev/null -m 3 -L -w ''%{http_code}'' $CANDLE_URL)" != "200" ]];\
+   do echo "Waiting for url" && sleep 2;\
+   done;\
+   echo "Server seems to be up: $CANDLE_URL"'
+
 
 
 totalk=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
 #logger total memory: $totalk
 
 if ls -l /dev/fb*; then
+
     # Start X server
     if [ -f $BOOT_DIR/candle_kiosk.txt ] && [ -f $BOOT_DIR/candle_first_run_complete.txt ]
     then
-      if [ "$totalk" -gt 1500000 ] || [ -f $BOOT_DIR/candle_kiosk_forced.txt ]
-      then
+
         echo "Candle: Enough memory to start X server" >> /dev/kmsg
         #logger Starting X
+
+        pkill vlc
+        pkill x
+        sleep 1
+
         if [ -n "$(ls /dev/input/by-id/usb-ILITEK_ILITEK-TP-mouse 2>/dev/null)" ]
         then
           # Raspad touchscreen
@@ -39,21 +57,20 @@ if ls -l /dev/fb*; then
           # any mouse
           if [ -n "$(ls /dev/input/by-id/*-mouse 2>/dev/null)" ]
           then
-            su - pi -c startx &
+            su - pi -c 'startx &'
             #su - pi -c 'unclutter -idle 10 -root -display :0 &'
           else
             if [ ! -f $BOOT_DIR/hide_mouse_pointer.txt ]
             then
-              su - pi    startx &
+              su - pi -c 'startx &'
             else
               su - pi -c 'startx -- -nocursor &'
               #su - pi -c 'unclutter -idle 0 -root -display :0 &'
             fi
           fi
         fi
-      fi
+      
     fi
 else
     echo "Candle: no display detected, not starting kiosk mode" >> /dev/kmsg
 fi
-
