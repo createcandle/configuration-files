37f3d4c122f4ac957e50d34db75d08cf87d773d4
flatsiedatsie <unfold@gmail.com>
2023-12-23 18:09:00 +0100

    Create splash_video.sh

---
+++ home/pi/candle/splash_video.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BOOT_DIR="/boot"
+if lsblk | grep -q $BOOT_DIR/firmware; then
+    BOOT_DIR="$BOOT_DIR/firmware"
+fi
+
+totalk=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
+#logger total memory: $totalk
+
+ if ls -l /dev/fb*; then
+   if [ -f /usr/bin/vlc ]; then
+    # Start X server
+    if [ -f $BOOT_DIR/candle_kiosk.txt ] && [ -f $BOOT_DIR/candle_first_run_complete.txt ]
+    then
+      if [ "$totalk" -gt 1500000 ] || [ -f $BOOT_DIR/candle_kiosk_forced.txt ]; then
+        if [ -f $BOOT_DIR/rotate180.txt ];l then
+          startx /usr/bin/vlc -I dummy -L -f --no-osd --no-audio --fullscreen --video-on-top --no-audio --video-filter='transform{type="180"}' $BOOT_DIR/splash.mp4 -- &
+      	else
+          startx /usr/bin/vlc -I dummy -L -f --no-osd --no-audio --fullscreen --video-on-top --no-audio $BOOT_DIR/splash.mp4 -- &
+      	fi
+      fi
+    fi
+  fi
+fi
