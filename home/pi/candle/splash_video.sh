57601f0139f6904cba3ad27b67541585dd9d5f5c
flatsiedatsie <unfold@gmail.com>
2024-01-13 23:11:20 +0100

    Update splash_video.sh

--- home/pi/candle/splash_video.sh
+++ home/pi/candle/splash_video.sh
@@ -26,16 +26,23 @@ else
   if ls -l /dev/fb* && [ -f $BOOT_DIR/splash.mp4 ] && [ -f /usr/bin/vlc ] && [ ! -f $BOOT_DIR/post_bootup_actions.sh ]; then
     if [ -f $BOOT_DIR/candle_kiosk.txt ] && [ -f $BOOT_DIR/candle_first_run_complete.txt ]; then
       if [ "$totalk" -gt 800000 ] || [ -f $BOOT_DIR/candle_kiosk_forced.txt ]; then
-        
+
+		echo "Candle: splash_video.sh: starting video" >> /dev/kmsg
+  
         if [ -f $BOOT_DIR/rotate180.txt ]; then
           /usr/bin/vlc -L -f --no-osd --no-audio --fullscreen --no-audio --video-filter='transform{type="180"}' --crop=4:3  $BOOT_DIR/splash.mp4
           #startx /usr/bin/vlc -I dummy -L -f --no-osd --no-audio --fullscreen --video-on-top --no-audio --video-filter='transform{type="180"}' $BOOT_DIR/splash.mp4 -- &
       	else
           /usr/bin/vlc -L -f --no-osd --no-audio --fullscreen --no-audio --crop=4:3  $BOOT_DIR/splash.mp4
       	fi
-       
+
+	  else
+   		echo "Candle: splash_video.sh: not starting video (low memory)" >> /dev/kmsg
       fi
+	  
     fi
+  else
+  	echo "Candle: splash_video.sh: not starting video (likely no /dev/fb*)" >> /dev/kmsg
   fi
   
 fi
