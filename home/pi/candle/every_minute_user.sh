8ce74ec16caf805920ed4c8eb2995c37d0c9d5e0
flatsiedatsie <unfold@gmail.com>
2025-09-26 13:58:06 +0200

    Update every_minute_user.sh

--- home/pi/candle/every_minute_user.sh
+++ home/pi/candle/every_minute_user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if cat /proc/cpuinfo | grep -q "Raspberry Pi 5" && [ -f /home/pi/candle/silence.mp3 ] && [ -f /usr/bin/pw-play ]; then
-    su -c '/usr/bin/pw-play --volume=0.01 /home/pi/candle/silence.mp3' pi
-fi
+#if cat /proc/cpuinfo | grep -q "Raspberry Pi 5" && [ -f /home/pi/candle/silence.mp3 ] && [ -f /usr/bin/pw-play ]; then
+#    su -c '/usr/bin/pw-play --volume=0.01 /home/pi/candle/silence.mp3' pi
+#fi
