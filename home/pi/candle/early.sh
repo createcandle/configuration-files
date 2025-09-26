34c29ed76601a83d7f248f57858eee301e3cff11
flatsiedatsie <unfold@gmail.com>
2025-09-26 16:49:34 +0200

    Update early.sh

--- home/pi/candle/early.sh
+++ home/pi/candle/early.sh
@@ -83,17 +83,22 @@ if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f "$BOOT_DIR/splash.png" ] &
 fi
 
 # Add firewall rules
-if iptables --list | grep 4443; then
-    echo "IPTABLES ALREADY ADDED"
+if [ -f /usr/sbin/iptables ] ; then
+	if iptables --list | grep 4443; then
+	    echo "IPTABLES ALREADY ADDED"
+	else
+	    echo "Candle early: adding iptable rules" >> /dev/kmsg
+	    iptables -t mangle -A PREROUTING -p tcp --dport 80 -j MARK --set-mark 1
+	    iptables -t mangle -A PREROUTING -p tcp --dport 443 -j MARK --set-mark 1
+	    iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+	    iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4443
+	    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -m mark --mark 1 -j ACCEPT
+	    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -m mark --mark 1 -j ACCEPT
+	fi
 else
-    echo "Candle early: adding iptable rules" >> /dev/kmsg
-    iptables -t mangle -A PREROUTING -p tcp --dport 80 -j MARK --set-mark 1
-    iptables -t mangle -A PREROUTING -p tcp --dport 443 -j MARK --set-mark 1
-    iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
-    iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4443
-    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -m mark --mark 1 -j ACCEPT
-    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -m mark --mark 1 -j ACCEPT
+	echo "Candle early: iptables not installed?" >> /dev/kmsg
 fi
+
 
 
 
