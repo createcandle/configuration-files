eb239c6a0200a94df313a831c321a372b3557ef4
flatsiedatsie <unfold@gmail.com>
2025-11-02 19:40:15 +0100

    Update prepare_for_disk_image.sh

--- home/pi/candle/prepare_for_disk_image.sh
+++ home/pi/candle/prepare_for_disk_image.sh
@@ -62,24 +62,6 @@ rm -rf /home/pi/.pki
 
 find /tmp -type f -atime +10 -delete
 
-echo "Well hello there" > /home/pi/.bash_history
-echo "curl -sSL www.candlesmarthome.com/tools/samba.txt | sudo bash" >> /home/pi/.bash_history
-echo "sudo fsck -n -f" >> /home/pi/.bash_history
-echo "journalctl --boot=0 --priority=0..3" >> /home/pi/.bash_history
-echo "systemctl list-units --failed" >> /home/pi/.bash_history
-echo "sudo service --status-all" >> /home/pi/.bash_history
-echo "sudo touch /boot/firmware/candle_rw_keep.txt" >> /home/pi/.bash_history
-echo "sudo touch /boot/firmware/candle_ssh.txt" >> /home/pi/.bash_history
-echo "avahi-browse -p -l -a -r -k -t" >> /home/pi/.bash_history
-echo "cat /sys/class/drm/card0/*HDMI*/status" >> /home/pi/.bash_history
-echo "aplay -l && arecord -l" >> /home/pi/.bash_history
-echo "wpctl status" >> /home/pi/.bash_history
-echo "sudo systemctl restart webthings-gateway.service" >> /home/pi/.bash_history
-echo "hostname -I" >> /home/pi/.bash_history
-#echo "sudo resolvconf -u" >> /home/pi/.bash_history
-#echo "cd /boot/firmware" >> /home/pi/.bash_history
-echo "tail -f -n10 ~/.webthings/log/run-app.log" >> /home/pi/.bash_history
-
 if [ -f /home/pi/.webthings/addons/power-settings/factory_reset.sh ] ; then
   touch $BOOT_DIR/developer.txt # will cause the real factory_reset script to also write zeroes to empty space. That script also calls SHUTDOWN, so nothing can be done after this.
   chmod +x /home/pi/.webthings/addons/power-settings/factory_reset.sh
