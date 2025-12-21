230277f3e28bb3cf9d7de582ea407ebf3f62f7ff
flatsiedatsie <unfold@gmail.com>
2025-12-21 18:39:54 +0100

    Update every_minute.sh

--- home/pi/candle/every_minute.sh
+++ home/pi/candle/every_minute.sh
@@ -32,6 +32,12 @@ if cat /home/pi/.webthings/etc/wpa_supplicant/wpa_supplicant.conf | grep -q psk=
 fi
 
 # limit the size of the dnsmasq log
+# limit the size of the dnsmasq log
 if [ -f /home/pi/dnsmasq_log.txt ]; then
-	tail -n 1000 /home/pi/dnsmasq_log.txt > tmp.txt && mv tmp.txt /home/pi/dnsmasq_log.txt
+	cp /home/pi/dnsmasq_log.txt /home/pi/dnsmasq_now.txt
+	chown pi:pi /home/pi/dnsmasq_now.txt
+fi
+echo "" > /home/pi/dnsmasq_log.txt
+if ps aux | grep -q 'dnsmasq -k -d'; then
+	kill -USR2 `pidof dnsmasq`
 fi
