0859492b2d211ad5745f311793e1a0edbd1bb14c
flatsiedatsie <unfold@gmail.com>
2025-12-21 13:21:33 +0100

    Update early.sh

--- home/pi/candle/early.sh
+++ home/pi/candle/early.sh
@@ -245,194 +245,12 @@ then
 fi
 
 
-# TODO: temporarily disabled to test if cookies can remain for login
-#if [ -d /home/pi/.webthings/chromium/BrowserMetrics ]; then
-#    rm -rf /home/pi/.webthings/chromium/BrowserMetrics/*
-#fi
 
-# Handle forced restore of Candle Controller backup
-if [ -f $BOOT_DIR/restore_controller_backup.txt ];
-then
-  rm $BOOT_DIR/restore_controller_backup.txt
-  echo "Detected $BOOT_DIR/restore_controller_backup.txt"
-  
-  if [ -f $BOOT_DIR/developer.txt ] || [ -f $BOOT_DIR/candle_cutting_edge.txt ]; then
-    systemctl start ssh.service
-  fi
-  
-  if [ -f /home/pi/controller_backup.tar ];
-  then
-  
-    # Show updating image
-    if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f "$BOOT_DIR/splash_updating.png" ] && [ -f "$BOOT_DIR/splash_updating180.png" ]; then
-      if [ -e "$BOOT_DIR/rotate180.txt" ]; then
-        /bin/ply-image $BOOT_DIR/splash_updating180.png
-      else
-        /bin/ply-image $BOOT_DIR/splash_updating.png
-      fi
-    fi
-    
-    # Restore backup
-    cd /home/pi || exit
-    echo "$(date) - forced restoring controller backup..." >> $BOOT_DIR/candle_log.txt
-    echo "$(date) - forced restoring controller backup..." >> /dev/kmsg
-    rm -rf /home/pi/webthings
-    tar -xvf controller_backup.tar
-    echo "$(date) - forced restoring controller backup done" >> $BOOT_DIR/candle_log.txt
-    echo "$(date) - forced restoring controller backup done" >> /dev/kmsg
-  
-  
-    if [ -f /home/pi/webthings/gateway/build/app.js ] \
-    && [ -f /home/pi/webthings/gateway/.post_upgrade_complete ] \
-    && [ -f /home/pi/webthings/gateway/build/static/index.html ] \
-    && [ -d /home/pi/webthings/gateway/node_modules ] \
-    && [ -d /home/pi/webthings/gateway/build/static/bundle ];
-    then
-      echo "$(date) - forced restoring controller backup done" >> $BOOT_DIR/candle_log.txt
-      echo "$(date) - forced restoring controller backup done" >> /dev/kmsg
-      reboot now
-      sleep 5
-      
-    else
-      echo "$(date) - forced restoring controller backup failed" >> $BOOT_DIR/candle_log.txt
-      echo "$(date) - forced restoring controller backup failed" >> /dev/kmsg
-      
-      # Show error image
-      if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f "$BOOT_DIR/error.png" ]; then
-        /bin/ply-image $BOOT_DIR/error.png
-      fi
-      
-      sleep 7200
-      exit 1
-    fi
-  
-  else
-    # Record that no backup was found
-    echo "$(date) - forced restoring controller backup: no backup found" >> $BOOT_DIR/candle_log.txt
-    echo "$(date) - forced restoring controller backup: no backup found" >> /dev/kmsg
-    
-    # Show error image
-    if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f "$BOOT_DIR/error.png" ]; then
-      /bin/ply-image $BOOT_DIR/error.png
-    fi
-    
-    sleep 7200
-    exit 1
-  fi
-
-
-# If the user wants a forced rebuild, do that.
-elif [ -f $BOOT_DIR/force_controller_rebuild.txt ];
-then
-  rm $BOOT_DIR/force_controller_rebuild.txt
-  echo "Detected $BOOT_DIR/force_controller_rebuild.txt"
-  
-  if [ -f $BOOT_DIR/developer.txt ] || [ -f $BOOT_DIR/candle_cutting_edge.txt ]; then
-    systemctl start ssh.service
-  fi
-  
-  if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f "$BOOT_DIR/splash_updating.png" ] && [ -f "$BOOT_DIR/splash_updating180.png" ]; then
-    if [ -e "$BOOT_DIR/rotate180.txt" ]; then
-      /bin/ply-image $BOOT_DIR/splash_updating180.png
-    else
-      /bin/ply-image $BOOT_DIR/splash_updating.png
-    fi
-  fi
-  
-  # Wait for IP address for at most 30 seconds
-  echo "Waiting for IP address..."
-  for i in {1..30}
-  do
-    #echo "current IP: $(hostname -I)"
-    if [ "$(hostname -I)" = "" ]
-    then
-      echo "Candle: early.sh: no network yet $i" >> /dev/kmsg
-      echo "no network yet $i"
-      sleep 1    
-    else
-      echo "Candle: early.sh: IP address detected: $(hostname -I)" >> /dev/kmsg
-      break
-    fi
-  done
-  
-  cd /home/pi || exit
-  echo "$(date) - starting forced controller regeneration..." >> $BOOT_DIR/candle_log.txt
-  echo "$(date) - starting forced controller regeneration..." >> /dev/kmsg
-
-
-    if [ -f $BOOT_DIR/candle_cutting_edge ]; then
-        wget https://raw.githubusercontent.com/createcandle/install-scripts/main/install_candle_controller.sh -O ./install_candle_controller.sh
-    else
-        curl -s https://api.github.com/repos/createcandle/install-scripts/releases/latest \
-        | grep "tarball_url" \
-        | cut -d : -f 2,3 \
-        | tr -d \" \
-        | sed 's/,*$//' \
-        | wget -qi - -O install-scripts.tar
-
-        tar -xf install-scripts.tar
-        rm install-scripts.tar
-
-        for directory in createcandle-install-scripts*; do
-          [[ -d $directory ]] || continue
-          echo "Directory: $directory"
-          mv -- "$directory" ./install-scripts
-        done
-
-        mv ./install-scripts/install_candle_controller.sh ./install_candle_controller.sh
-        rm -rf ./install-scripts
-        #echo
-        #echo "result:"
-        #ls install_candle_controller.sh
-    fi
-
-
-    # Check if the install_candle_controller.sh file now exists
-    if [ ! -f install_candle_controller.sh ]; then
-        echo
-        echo "ERROR, missing install_candle_controller.sh file"
-        echo "$(date) - Failed to download install_candle_controller script" >> $BOOT_DIR/candle_log.txt
-        echo
-
-        # Show error image
-        if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f $BOOT_DIR/error.png ]; then
-            /bin/ply-image $BOOT_DIR/error.png
-            sleep 7200
-        fi
-
-        exit 1
-    fi
-
-    chmod +x ./install_candle_controller.sh
-    sudo -u pi ./install_candle_controller.sh
-    wait
-    rm ./install_candle_controller.sh
-
-
-  if [ -f /home/pi/webthings/gateway/build/app.js ] \
-  && [ -f /home/pi/webthings/gateway/.post_upgrade_complete ] \
-  && [ -f /home/pi/webthings/gateway/build/static/index.html ] \
-  && [ -d /home/pi/webthings/gateway/node_modules ] \
-  && [ -d /home/pi/webthings/gateway/build/static/bundle ];
-  then
-    echo "$(date) - forced controller regeneration done" >> $BOOT_DIR/candle_log.txt
-    echo "$(date) - forced controller regeneration done" >> /dev/kmsg
-    reboot now
-    sleep 5
-      
-  else
-    echo "$(date) - forced controller regeneration failed" >> $BOOT_DIR/candle_log.txt
-    echo "$(date) - forced controller regeneration failed" >> /dev/kmsg
-    
-    # Show error image
-    if [ -e "/bin/ply-image" ] && [ -e /dev/fb0 ] && [ -f "$BOOT_DIR/error.png" ]; then
-      /bin/ply-image $BOOT_DIR/error.png
-    fi
-    sleep 7200
-    exit 1
-  fi
-
+if [ -d /home/pi/.webthings/chromium/BrowserMetrics ]; then
+    rm -rf /home/pi/.webthings/chromium/BrowserMetrics/*
 fi
+
+
 
 
 
@@ -458,8 +276,8 @@ fi
 if [ -f /home/pi/candle/files_check.sh ]; then
   if [ -n "$(/home/pi/candle/files_check.sh)" ]; then
     /home/pi/candle/files_check.sh > $BOOT_DIR/candle_missing_files.txt
-  elif [ -f $BOOT_DIR/candle_issues.txt ]; then
-    rm $BOOT_DIR/candle_issues.txt
+  elif [ -f $BOOT_DIR/candle_missing_files.txt ]; then
+    rm $BOOT_DIR/candle_missing_files.txt
   fi
 fi
 
