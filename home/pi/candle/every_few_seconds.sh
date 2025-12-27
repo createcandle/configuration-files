f9a4db5662802ec1d0c07588d129332500371a4a
flatsiedatsie <unfold@gmail.com>
2025-12-27 16:36:24 +0100

    Update every_few_seconds.sh

--- home/pi/candle/every_few_seconds.sh
+++ home/pi/candle/every_few_seconds.sh
@@ -5,7 +5,10 @@ if [ -f /home/pi/dnsmasq_log.txt ]; then
 	cp /home/pi/dnsmasq_log.txt /home/pi/dnsmasq_now.txt
 	chown pi:pi /home/pi/dnsmasq_now.txt
 fi
-echo "" > /home/pi/dnsmasq_log.txt
+if [ -f /home/pi/dnsmasq_now.txt ]; then
+	chown pi:pi /home/pi/dnsmasq_now.txt
+fi
+#echo "" > /home/pi/dnsmasq_log.txt
 if ps aux | grep -q 'dnsmasq -k -d'; then
 	kill -USR2 `pidof dnsmasq`
 fi
