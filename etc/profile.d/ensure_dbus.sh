8ce4173f83988a511104c9ded1d20bd42f8e15b5
flatsiedatsie <unfold@gmail.com>
2026-02-24 15:28:14 +0100

    Refactor DBus session handling in ensure_dbus.sh
    Refactor ensure_dbus.sh to start DBus session bus if not running and save environment variables.

--- etc/profile.d/ensure_dbus.sh
+++ etc/profile.d/ensure_dbus.sh
@@ -1,29 +1,16 @@
+echo ""
+echo "__CHECK BEFORE:__"
+declare -p | grep DBUS
+
 if [ -z "${DBUS_SESSION_BUS_PID}" ] && [ -e "/var/run/dbus/sessionbus.pid" ]; then
 	DBUS_SESSION_BUS_PID=$(cat /var/run/dbus/sessionbus.pid)
 fi
 if [ -z "${DBUS_SESSION_BUS_ADDRESS}" ] && [ -e "/var/run/dbus/sessionbus.address" ]; then
 	DBUS_SESSION_BUS_ADDRESS=$(cat /var/run/dbus/sessionbus.address)
 fi
-# start the dbus as session bus and save the enviroment vars
-#if [ -z ${DBUS_SESSION_BUS_PID+x} ]; then
-if [ -z "${DBUS_SESSION_BUS_PID}" ]; then
-  echo ensure_dbus: starting session dbus
-  #eval "export $(/usr/bin/dbus-launch)"
-  /usr/bin/dbus-launch | while read line; do
-    line=$(echo "$line" | tr -d '\n')
-    echo "ensure_dbus: exporting: -->$line<--"
-    export "$line"
-  done
-  #echo "${DBUS_SESSION_BUS_PID}">/var/run/dbus/sessionbus.pid
-  echo "${DBUS_SESSION_BUS_PID}" | sudo tee /var/run/dbus/sessionbus.pid
-  #echo "${DBUS_SESSION_BUS_ADDRESS}">/var/run/dbus/sessionbus.address
-  echo "${DBUS_SESSION_BUS_ADDRESS}" | sudo tee /var/run/dbus/sessionbus.address
-  #echo ensure_dbus: session dbus now runs at pid="${DBUS_SESSION_BUS_PID}"
-  echo "ensure_dbus: session dbus now runs at pid=$DBUS_SESSION_BUS_PID"
-  
-else
-  echo "ensure_dbus: session dbus already runs at pid=$DBUS_SESSION_BUS_PID"
-fi
+
+
+
 
 if [ -z "$XDG_RUNTIME_DIR" ]; then
   XDG_RUNTIME_DIR="/run/user/$(id -u)"
@@ -37,3 +24,44 @@ if [[ -z "$DISPLAY" ]]; then
   export DISPLAY=$HOSTNAME:0.0
   echo ensure_dbus: had to create DISPLAY, it is now: "${DISPLAY}"
 fi
+
+# start the dbus as session bus and save the environment vars
+#if [ -z ${DBUS_SESSION_BUS_PID+x} ]; then
+if [ -z "${DBUS_SESSION_BUS_PID}" ]; then
+  echo ensure_dbus: starting new session dbus
+
+  eval $(dbus-launch --sh-syntax)
+
+  echo "--eval done--"
+  echo ""
+  echo "DBUS_SESSION_BUS_PID: $DBUS_SESSION_BUS_PID"
+  echo "DBUS_SESSION_BUS_ADDRESS: $DBUS_SESSION_BUS_ADDRESS"
+  echo ""
+  echo "--check done--"
+  echo ""
+  #echo "${DBUS_SESSION_BUS_PID}">/var/run/dbus/sessionbus.pid
+  if [ -n "${DBUS_SESSION_BUS_PID}" ]; then
+    echo "OK, DBUS_SESSION_BUS_PID was set: $DBUS_SESSION_BUS_PID"
+  	echo "${DBUS_SESSION_BUS_PID}" | sudo tee /var/run/dbus/sessionbus.pid
+  else
+    echo "ERROR, DBUS_SESSION_BUS_PID was empty!: -->$DBUS_SESSION_BUS_PID<--"
+  fi
+	
+  #echo "${DBUS_SESSION_BUS_ADDRESS}">/var/run/dbus/sessionbus.address
+  if [ -n "${DBUS_SESSION_BUS_ADDRESS}" ]; then
+    echo "OK, DBUS_SESSION_BUS_ADDRESS was set: $DBUS_SESSION_BUS_ADDRESS"
+    echo "${DBUS_SESSION_BUS_ADDRESS}" | sudo tee /var/run/dbus/sessionbus.address
+  else
+    echo "ERROR, DBUS_SESSION_BUS_ADDRESS was empty!: -->$DBUS_SESSION_BUS_ADDRESS<--"
+  fi
+  #echo ensure_dbus: session dbus now runs at pid="${DBUS_SESSION_BUS_PID}"
+  #echo "ensure_dbus: session dbus now runs at pid=-->$DBUS_SESSION_BUS_PID<--"
+  
+else
+  echo "ensure_dbus: session dbus already runs at pid=-->$DBUS_SESSION_BUS_PID<--"
+fi
+
+# show all varibles:
+echo ""
+echo "__CHECK AFTER:__"
+declare -p | grep DBUS
