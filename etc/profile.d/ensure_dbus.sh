cae415bcb4bade51921ed6bbceeeebd3d2fc0a81
flatsiedatsie <unfold@gmail.com>
2026-03-02 12:25:04 +0100

    Update ensure_dbus.sh

--- etc/profile.d/ensure_dbus.sh
+++ etc/profile.d/ensure_dbus.sh
@@ -2,7 +2,6 @@
 
 if [ -n "$DBUS_SESSION_BUS_ADDRESS" ] ; then
   SESSION_FILE=$(ls -tp /home/pi/.dbus/session-bus | grep -v /$ | head -1)
-  echo $SESSION_FILE
   
   if ps aux | grep -q '/dbus-daemon'; then
     if [ -n "$SESSION_FILE" ] && [ -f "/home/pi/.dbus/session-bus/$SESSION_FILE" ]; then
