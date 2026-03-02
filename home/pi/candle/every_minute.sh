45844051757fd9fb06ac173cec8d338f4c40dd1c
flatsiedatsie <unfold@gmail.com>
2026-03-02 07:44:17 +0100

    Update every_minute.sh

--- home/pi/candle/every_minute.sh
+++ home/pi/candle/every_minute.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if [ -f /boot/firmware/emergency.txt ]; then
+	exit 0
+fi
+
 # Fix avahi sometimes appending "-2" or "-3" to its hostname is a Bonjour server is repeating it on the network, causing it to think the name already exists
 HOSTNAME=$(hostname)
 AVAHI_HOSTNAME=$(systemctl status avahi-daemon.service | grep "avahi-daemon: running \[" | cut -d "[" -f2| cut -d "]" -f1 | cut -d. -f1) 
 #if [ "$AVAHI_HOSTNAME" != "$HOSTNAME" ] && [ "$HOSTNAME" != "candle" ]; then
 if [ "$AVAHI_HOSTNAME" != "$HOSTNAME" ]; then
     echo "Hostname mismatch (hostname:$HOSTNAME, avahi:$AVAHI_HOSTNAME). Restarting avahi."
+	echo "Hostname mismatch (hostname:$HOSTNAME, avahi:$AVAHI_HOSTNAME). Restarting avahi." >> /dev/kmsg
     systemctl restart avahi-daemon.service
 fi
 
