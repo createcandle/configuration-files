5997c27f363cdf401dcc1dc0f1071f6ca784bf68
flatsiedatsie <unfold@gmail.com>
2026-03-04 18:50:22 +0100

    Update ensure_dbus.sh

--- etc/profile.d/ensure_dbus.sh
+++ etc/profile.d/ensure_dbus.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+if [ -n "$XDG_RUNTIME_DIR" ] ; then
+  export XDG_RUNTIME_DIR=/run/user/$(id -u)
+fi
+
 if [ -n "$DBUS_SESSION_BUS_ADDRESS" ] ; then
   SESSION_FILE=$(ls -tp /home/pi/.dbus/session-bus | grep -v /$ | head -1)
-  
   if ps aux | grep -q '/dbus-daemon'; then
     if [ -n "$SESSION_FILE" ] && [ -f "/home/pi/.dbus/session-bus/$SESSION_FILE" ]; then
-        source "/home/pi/.dbus/session-bus/$SESSION_FILE"
+      source "/home/pi/.dbus/session-bus/$SESSION_FILE"
     fi
   fi
 fi
 
 if [ -n "$DISPLAY"] ; then
-	export DISPLAY=:0
+  export DISPLAY=:0
 fi
+
+export CANDLE=COOL
