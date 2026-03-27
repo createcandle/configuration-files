b371668476fba62351245c1abf8460f548cfbe35
flatsiedatsie <unfold@gmail.com>
2026-03-27 10:32:05 +0100

    Update candle_first_run.sh

--- home/pi/candle/candle_first_run.sh
+++ home/pi/candle/candle_first_run.sh
@@ -129,7 +129,7 @@ fi
 
 openssl req -new -sha256 -key "${SSL_DIR}/privatekey.pem" -out "${SSL_DIR}/csr.pem" -subj '/CN=CandleSmartHome.com/O=Candle Controller/C=US'
 openssl x509 -req -days 3650 -in "${SSL_DIR}/csr.pem" -signkey "${SSL_DIR}/privatekey.pem" -out "${SSL_DIR}/certificate.pem"
-
+openssl x509 -outform der -in "${SSL_DIR}/certificate.pem" -out "${SSL_DIR}/certificate.crt"
 
 
 chown -R pi:pi "${SSL_DIR}"
