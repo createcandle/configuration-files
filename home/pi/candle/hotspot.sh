7500cd93a7c6737d67b60f78ae85065f0abe08e0
flatsiedatsie <unfold@gmail.com>
2026-04-07 15:23:25 +0200

    Update hotspot.sh

--- home/pi/candle/hotspot.sh
+++ home/pi/candle/hotspot.sh
@@ -13,6 +13,18 @@ if [ -f $BOOT_DIR/candle_emergency_hotspot.txt ]; then
 	exit 0
 fi
 
+# Generate a random ip4/16 octet. The aim is that each hotspot has a separate range.
+# In the future they could more easily be meshed together in a virtual private network between the Cnadle COntrollers
+# As a way to create a larger wifi network with multiple access points that is still walled off from the rest of the network
+NETID=$((20 + $RANDOM % 220))
+if [ -f $BOOT_DIR/candle_hotspot_net_number.txt ]; then
+	NETID=$(cat "$BOOT_DIR/candle_hotspot_net_number.txt" | tr -d '\n')
+else
+	echo $NETID > $BOOT_DIR/candle_hotspot_net_number.txt
+fi
+
+echo "hotspot: hopefully unique random NETID: $NETID"
+
 # most basc command to create a hotspot:
 # nmcli dev wifi hotspot ifname uap0 ssid "Candle" password "smarthome"
 
@@ -86,8 +98,9 @@ start_dnsmasq () {
 		echo "hotspot.sh: bringing up hotspot and starting dnsmasq"
 		
 		
-		IP4S=$(hostname -I | sed -r 's/192.168.12.1//' | xargs)
-		echo "hotspot.sh: IPv4 address(es): $IP4S"
+		#IP4S=$(hostname -I | sed -r 's/172.16.$NETID.1//' | xargs)
+		IPS=$(hostname -I | sed -r 's/172.16.[0-9]\+.1//' | xargs)
+		echo "hotspot.sh: IP address(es): $IPS"
 	
 		if iptables -t nat -L -v | grep -q "uap0"; then
 			echo "uap0 already exists in iptables"
@@ -102,16 +115,16 @@ start_dnsmasq () {
 		
 			if [ ! -f $BOOT_DIR/candle_hotspot_allow_access_to_main_network.txt ]; then
 				echo "blocking access of hotspot network to main network"
-				#iptables -A FORWARD -d 192.168.2.2 -m iprange --src-range 192.168.12.2-192.168.12.255 -j DROP
+				#iptables -A FORWARD -d 172.16.2.2 -m iprange --src-range 172.16.12.2-172.16.12.255 -j DROP
 				#iptables -I INPUT -i uap0 -d <main_network_IP> -j DROP
 			
-				#iptables -A FORWARD -i uap0 -m iprange --src-range 192.168.12.2-192.168.12.255 -o eth0 -d 192.168.0.0/16 -j DROP
-				#iptables -A FORWARD -i uap0 -m iprange --src-range 192.168.12.2-192.168.12.255 -o wlan0 -d 192.168.0.0/16 -j DROP
+				#iptables -A FORWARD -i uap0 -m iprange --src-range 172.16.12.2-172.16.12.255 -o eth0 -d 172.16.0.0/16 -j DROP
+				#iptables -A FORWARD -i uap0 -m iprange --src-range 172.16.12.2-172.16.12.255 -o wlan0 -d 172.16.0.0/16 -j DROP
 			fi
 		
 
 			# Force all DNS traffic on the hotspot network to go to/through the Candle Controller
-			iptables -t nat -A PREROUTING -i uap0 -s 192.168.12.0/24 -p udp --dport 53 -j DNAT --to-destination 192.168.12.1:53
+			iptables -t nat -A PREROUTING -i uap0 -s 172.16.12.0/24 -p udp --dport 53 -j DNAT --to-destination 172.16.$NETID.1:53
 			#ip6tables -t nat -A PREROUTING -i uap0 -s fd00:12::/8 -p udp --dport 53 -j DNAT --to-destination fd00:12::1
 			ip6tables -t nat -A PREROUTING -i uap0 -p udp --dport 53 -j DNAT --to-destination fd00:12::1
 		
@@ -124,38 +137,38 @@ start_dnsmasq () {
 			#iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 
 			echo "candle: hotspot.sh: enablng masquerade"
-			iptables -t nat -A POSTROUTING -s 192.168.12.0/24 ! -d 192.168.12.0/24  -j MASQUERADE
+			iptables -t nat -A POSTROUTING -s 172.16.12.0/24 ! -d 172.16.12.0/24  -j MASQUERADE
 			ip6tables -t nat -A POSTROUTING -s ff02::1 ! -d ff02::1  -j MASQUERADE
 
 
 			echo "candle: hotspot.sh: step 1 of adding port redirect rules on hotspot side"
-			iptables -I PREROUTING -p tcp -i uap0 -s 192.168.12.0/24 -d 192.168.12.1/32 --dport 80 -j REDIRECT --to-port 8080
-			iptables -I PREROUTING -p tcp -i uap0 -s 192.168.12.0/24 -d 192.168.12.1/32 --dport 443 -j REDIRECT --to-port 4443
+			iptables -I PREROUTING -p tcp -i uap0 -s 172.16.12.0/24 -d 172.16.$NETID.1/32 --dport 80 -j REDIRECT --to-port 8080
+			iptables -I PREROUTING -p tcp -i uap0 -s 172.16.12.0/24 -d 172.16.$NETID.1/32 --dport 443 -j REDIRECT --to-port 4443
 
-			ip6tables -I PREROUTING -p tcp -i uap0 -s fd00:12::/8 -d fd00:12::1 --dport 80 -j REDIRECT --to-port 8080
-			ip6tables -I PREROUTING -p tcp -i uap0 -s fd00:12::/8 -d fd00:12::1 --dport 443 -j REDIRECT --to-port 4443
+			ip6tables -I PREROUTING -p tcp -i uap0 -s "fd00:$NETID::/8" -d "fd00:$NETID::1" --dport 80 -j REDIRECT --to-port 8080
+			ip6tables -I PREROUTING -p tcp -i uap0 -s "fd00:$NETID::/8" -d "fd00:$NETID::1" --dport 443 -j REDIRECT --to-port 4443
 		
 	
 			# Block access to parent local networks
-			if [ ! -f $BOOT_DIR/candle_hotspot_allow_traversal.txt ]; then
+			if [ ! -f $BOOT_DIR/candle_hotspot_allow_access_to_main_network.txt ]; then
 				echo "candle: hotspot.sh: blocking traversal to local network"
-				iptables -I FORWARD -i uap0 -d 192.168.0.0/16 -m iprange --src-range 192.168.12.2-192.168.12.255 -j DROP
-				iptables -I FORWARD -i uap0 -d 172.16.0.0/12 -m iprange --src-range 192.168.12.2-192.168.12.255 -j DROP
-				iptables -I FORWARD -i uap0 -d 10.0.0.0/8 -m iprange --src-range 192.168.12.2-192.168.12.255 -j DROP
+				iptables -I FORWARD -i uap0 -d 172.16.0.0/16 -m iprange --src-range "172.16.$NETID.2-172.16.$NETID.255" -j DROP
+				iptables -I FORWARD -i uap0 -d 172.16.0.0/12 -m iprange --src-range "172.16.$NETID.2-172.16.$NETID.255" -j DROP
+				iptables -I FORWARD -i uap0 -d 10.0.0.0/8 -m iprange --src-range "172.16.$NETID.2-172.16.$NETID.255" -j DROP
 
-				ip6tables -I FORWARD -i uap0 -d fe80::/10 -s fd00:12::/8 -j DROP
+				ip6tables -I FORWARD -i uap0 -d fe80::/10 -s "fd00:$NETID::/8" -j DROP
 			
 			fi
 
 		
 			# Redirect Candle UI ports
 			echo "candle: hotspot.sh: step 2 of adding port redirect rules on hotspot side"
-			iptables -t mangle -I PREROUTING -s 192.168.12.0/24 -p tcp -d 192.168.12.1 --dport 80 -j MARK --set-mark 2
-			iptables -t mangle -I PREROUTING -s 192.168.12.0/24 -p tcp -d 192.168.12.1 --dport 443 -j MARK --set-mark 2
-			iptables -t nat -I PREROUTING -s 192.168.12.0/24 -p tcp -d 192.168.12.1 --dport 80 -j REDIRECT --to-port 8080
-			iptables -t nat -I PREROUTING -s 192.168.12.0/24 -p tcp -d 192.168.12.1 --dport 443 -j REDIRECT --to-port 4443
-			iptables -I INPUT -s 192.168.12.0/24 -m state --state NEW -m tcp -p tcp -d 192.168.12.1 --dport 8080 -m mark --mark 2 -j ACCEPT
-			iptables -I INPUT -s 192.168.12.0/24 -m state --state NEW -m tcp -p tcp -d 192.168.12.1 --dport 4443 -m mark --mark 2 -j ACCEPT
+			iptables -t mangle -I PREROUTING -s "172.16.$NETID.0/24" -p tcp -d "172.16.$NETID.1" --dport 80 -j MARK --set-mark 2
+			iptables -t mangle -I PREROUTING -s "172.16.$NETID.0/24" -p tcp -d "172.16.$NETID.1" --dport 443 -j MARK --set-mark 2
+			iptables -t nat    -I PREROUTING -s "172.16.$NETID.0/24" -p tcp -d "172.16.$NETID.1" --dport 80 -j REDIRECT --to-port 8080
+			iptables -t nat    -I PREROUTING -s "172.16.$NETID.0/24" -p tcp -d "172.16.$NETID.1" --dport 443 -j REDIRECT --to-port 4443
+			iptables -I INPUT -s "172.16.$NETID.0/24" -m state --state NEW -m tcp -p tcp -d "172.16.$NETID.1" --dport 8080 -m mark --mark 2 -j ACCEPT
+			iptables -I INPUT -s "172.16.$NETID.0/24" -m state --state NEW -m tcp -p tcp -d "172.16.$NETID.1" --dport 4443 -m mark --mark 2 -j ACCEPT
 
 		fi
 		
@@ -196,24 +209,24 @@ start_dnsmasq () {
 				echo "candle: hotspot.sh: not starting time server"
 			elif [ -f /home/pi/candle/time_server.py ]; then
 
-				if ip addr show uap0 | grep -q '192.168.12.1'; then
-					if ps aux | grep 'python3 /home/pi/candle/time_server.py' | grep -q '192.168.12.1 123'; then
+				if ip addr show uap0 | grep -q '172.16.$NETID.1'; then
+					if ps aux | grep 'python3 /home/pi/candle/time_server.py' | grep -q "172.16.$NETID.1 123"; then
 						echo "timeserver is already running"
 					else
 						echo "starting NTP server"
 				
-						if iptables -t nat -L -v | grep -q "192.168.12.1:123"; then
+						if iptables -t nat -L -v | grep -q "172.16.$NETID.1:123"; then
 							echo "NTP server iptables rule seems to already exist"
 						else
 							echo "adding NTP server iptables rule"
-							iptables -t nat -A PREROUTING -i uap0 -p udp --dport 123 -j DNAT --to-destination 192.168.12.1:123
+							iptables -t nat -A PREROUTING -i uap0 -p udp --dport 123 -j DNAT --to-destination "172.16.$NETID.1:123"
 						fi
-						python3 /home/pi/candle/time_server.py 192.168.12.1 123 &
+						python3 /home/pi/candle/time_server.py "172.16.$NETID.1" 123 &
 					fi
 
 
 				else
-					echo "ERROR, uap0 interface does not have 192.168.12.1 ip address (yet). Cannot start NTP server. ifconfig:"
+					echo "ERROR, uap0 interface does not have 172.16.$NETID.1 ip address (yet). Cannot start NTP server. ifconfig:"
 				fi
 				
 				
@@ -223,6 +236,25 @@ start_dnsmasq () {
 				
 				if nmcli connection show --active | grep -q Candle_hotspot; then
 					echo "FINAL STEP: STARTING DNSMASQ"
+					# 172.16.12.
+					#sed -i -E -e "s|127\.0\.1\.1[ \t]+.*|127\.0\.1\.1 \t$hostname|" /home/pi/.webthings/etc/hosts
+					if [ -f /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf ]; then
+						if cat /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf | grep -q "172.16.$NETID."; then
+							echo "local-DNS.conf seems to already have been updated with the current NETID: $NETID"
+						else
+							echo "local-DNS.conf needs to be updated to use the current NETID: $NETID"
+							cat /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf | sed -E "s/172.16.([0-9]+)./172.16.$NETID./g" > /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf.updated
+							if [ -f /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf.updated ]; then
+								if [ -s /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf.updated ]; then
+									echo "copying updated local-DNS.conf over the old version"
+									cp /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf.updated /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf
+								fi
+								rm /home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf.updated
+							else
+								echo "ERROR, home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf.updated did not exist"
+							fi
+						fi
+					fi
 					dnsmasq -k -d --no-daemon --conf-file=/home/pi/.webthings/etc/NetworkManager/dnsmasq.d/local-DNS.conf 1> /dev/null
 				fi
 				
@@ -269,8 +301,8 @@ if ip link show | grep -q "mlan0:" ; then
 		echo "uap0 does not exist yet"
 		/sbin/iw dev mlan0 interface add uap0 type __ap
 		sleep 1
-		#ip address add 192.168.12.1/24 dev uap0
-		#ifconfig uap0 192.168.12.1 netmask 255.255.255.0
+		#ip address add 172.16.$NETID.1/24 dev uap0
+		#ifconfig uap0 172.16.$NETID.1 netmask 255.255.255.0
 		#ip -6 addr add fd00:12::1 dev uap0
 		#iw dev uap0 set power_save off
 		#sleep 1
@@ -284,8 +316,8 @@ elif ip link show | grep -q "wlan0:" ; then
 		echo "uap0 does not exist yet"
 		/sbin/iw dev wlan0 interface add uap0 type __ap
 		sleep 1
-		#ip address add 192.168.12.1/24 dev uap0
-		#ifconfig uap0 192.168.12.1 netmask 255.255.255.0
+		#ip address add 172.16.$NETID.1/24 dev uap0
+		#ifconfig uap0 172.16.$NETID.1 netmask 255.255.255.0
 		#ip -6 addr add fd00:12::1 dev uap0
 		#iw dev uap0 set power_save off
 		#sleep 1
@@ -453,11 +485,11 @@ if ip link show | grep -q "uap0:"; then
 		ifconfig uap0
 		echo
 		
-		#if ifconfig uap0 | grep -q '192.168.12.1'; then
+		#if ifconfig uap0 | grep -q '172.16.$NETID.1'; then
 		#	echo "OK, uap0 still has the correct IP address"
 		#else
 		#	echo "forcing ip address for uap0 again"
-		#	ip address add 192.168.12.1/24 dev uap0
+		#	ip address add 172.16.$NETID.1/24 dev uap0
 		#fi	
 		
 		
@@ -488,7 +520,7 @@ if ip link show | grep -q "uap0:"; then
                     ifname uap0 \
                     type wifi \
                     autoconnect yes \
-                    ipv4.method manual ipv4.addresses "192.168.12.1/24" \
+                    ipv4.method manual ipv4.addresses "172.16.$NETID.1/24" \
                     802-11-wireless.band bg \
                     802-11-wireless.channel 1 \
                     802-11-wireless.mode ap \
@@ -508,12 +540,12 @@ if ip link show | grep -q "uap0:"; then
                     ifname uap0 \
                     type wifi \
                     autoconnect yes \
-                    ipv4.method manual ipv4.addresses "192.168.12.1/24" \
+                    ipv4.method manual ipv4.addresses "172.16.$NETID.1/24" \
                     802-11-wireless.band bg \
                     802-11-wireless.channel 1 \
                     802-11-wireless.mode ap \
                     802-11-wireless.ssid "$SSID" \
-                    ipv6.method manual ipv6.addresses 'fd00:12::1/8'
+                    ipv6.method manual ipv6.addresses "fd00:$NETID::1/8"
 				
 			fi
 			
@@ -524,13 +556,13 @@ if ip link show | grep -q "uap0:"; then
 			#nmcli con modify Candle_hotspot wifi-sec.pmf disable
 			nmcli con modify Candle_hotspot 802-11-wireless-security.pmf optional
 			
-			nmcli connection modify Candle_hotspot ipv4.gateway "192.168.12.1" 
-			nmcli connection modify Candle_hotspot ipv4.dns "192.168.12.1" ipv4.dns-priority 1000
+			nmcli connection modify Candle_hotspot ipv4.gateway "172.16.$NETID.1" 
+			nmcli connection modify Candle_hotspot ipv4.dns "172.16.$NETID.1" ipv4.dns-priority 1000
 			nmcli connection modify Candle_hotspot ipv4.never-default true
 		
 			#nmcli connection modify Candle_hotspot ipv6.addresses 'fd00::/8' ipv6.method manual 
-			nmcli connection modify Candle_hotspot ipv6.gateway 'fd00:12::1' 
-			nmcli connection modify Candle_hotspot ipv6.dns 'fd00:12::1' ipv6.dns-priority 1000
+			nmcli connection modify Candle_hotspot ipv6.gateway "fd00:$NETID::1"
+			nmcli connection modify Candle_hotspot ipv6.dns "fd00:$NETID::1" ipv6.dns-priority 1000
 			nmcli connection modify Candle_hotspot ipv6.never-default true
 			#nmcli connection modify Candle_hotspot ipv6.method "ignore"
 		
@@ -549,7 +581,7 @@ if ip link show | grep -q "uap0:"; then
 		#nmcli connection add type wifi ifname uap0 con-name Candle_hotspot
 	
 		#nmcli con add con-name Candle_hotspot ifname uap0 type dummy
-		#nmcli con add con-name Candle_hotspot ifname uap0 type wifi connection.autoconnect yes 802-11-wireless.ssid "$SSID" ipv4.method manual ipv4.addresses "192.168.12.1/24" ipv6.method manual ipv6.addresses 'fd00::/8' 802-11-wireless.band bg 802-11-wireless.channel 1
+		#nmcli con add con-name Candle_hotspot ifname uap0 type wifi connection.autoconnect yes 802-11-wireless.ssid "$SSID" ipv4.method manual ipv4.addresses "172.16.$NETID.1/24" ipv6.method manual ipv6.addresses 'fd00::/8' 802-11-wireless.band bg 802-11-wireless.channel 1
 		#nmcli con add type wifi ifname wlan0 con-name <your_hotspot_name> autoconnect yes ssid <your_ssid>
 		
 		
@@ -558,7 +590,7 @@ if ip link show | grep -q "uap0:"; then
 		#nmcli connection modify Candle_hotspot 802-11-wireless.band bg 802-11-wireless.channel 1
 		
 		
-		#nmcli connection modify Candle_hotspot ipv4.addresses "192.168.12.1/24" ipv4.method manual 
+		#nmcli connection modify Candle_hotspot ipv4.addresses "172.16.$NETID.1/24" ipv4.method manual 
 		
 		
 		#nmcli connection modify Candle_hotspot connection.interface-name uap0
@@ -708,10 +740,7 @@ if ip link show | grep -q "uap0:"; then
 					
 				fi
 				
-				
 			fi
-				
-			
 			
 		else
 			echo "ERROR, failed to create hotspot connection?"
