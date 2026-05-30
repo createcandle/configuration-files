da63b92a77cf90e2b171d6716da6aa295a59dc38
flatsiedatsie <unfold@gmail.com>
2026-05-30 19:41:59 +0200

    Update candle_first_run_user.sh

--- home/pi/candle/candle_first_run_user.sh
+++ home/pi/candle/candle_first_run_user.sh
@@ -4,11 +4,11 @@
 VIRTUAL_SINK=$(wpctl status | grep '\[Audio/Sink\]' | grep "virtual_combine_all_sinks")
 echo "pipewire VIRTUAL_SINK: $VIRTUAL_SINK"
 if [ -n "$VIRTUAL_SINK" ]; then
-	if echo "$VIRTUAL_SINK" | grep -q '*'; then
+	if echo "$VIRTUAL_SINK" | grep -q '\*'; then
 		echo "virtual audio sink already seems to be the default"
 	else
 		echo "changing pipewire default audio output to VIRTUAL_SINK: $VIRTUAL_SINK"
-		SINK_ID=$(wpctl status | grep '\[Audio/Sink\]' | grep "virtual_combine_all_sinks" | egrep '^ │( )*[0-9]*' -o | cut -c6-55 | egrep -o '[0-9]*')
+		SINK_ID=$(wpctl status | grep '\[Audio/Sink\]' | grep "virtual_combine_all_sinks" | grep -E '^ │( )*[0-9]*' -o | cut -c6-55 | grep -E -o '[0-9]*')
 		if [[ $SINK_ID =~ ^[0-9]+$ ]]; then
 			echo "setting pipewire virtual sink as the default.  SINK_ID: $SINK_ID"
 			wpctl set-default "$SINK_ID"
