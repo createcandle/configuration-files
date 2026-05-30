b714211833538aafa07525fd768f3cb701c24b5e
flatsiedatsie <unfold@gmail.com>
2026-05-30 19:29:33 +0200

    Update every_hour.sh

--- home/pi/candle/every_hour.sh
+++ home/pi/candle/every_hour.sh
@@ -2,7 +2,7 @@
 
 # This script is run as root
 
-if [ cat /proc/device-tree/model | grep -q "Microsoft Surface RT" ]; then
+if cat /proc/device-tree/model | grep -q "Microsoft Surface RT"; then
   if [ -f /boot/firmware/candle_developer.txt ]; then
     echo "Candle: every_hour: preventively turning WiFi off and on again to avoid WiFi issues on Surface RT" >> /dev/kmsg
   fi
