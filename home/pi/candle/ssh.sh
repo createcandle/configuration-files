cd3eca7e3e5616a6de96d3121a767a42d008a50c
flatsiedatsie <unfold@gmail.com>
2026-05-31 17:39:56 +0200

    Update ssh.sh

--- home/pi/candle/ssh.sh
+++ home/pi/candle/ssh.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-if [ -f /boot/candle_ssh.txt ] || [ -f /boot/candle_ssh_keep.txt ] || [ -f /boot/firmware/candle_ssh.txt ] || [ -f /boot/firmware/candle_ssh_keep.txt ]; then
+BOOT_DIR="/boot"
+if lsblk | grep -q $BOOT_DIR/firmware; then
+    BOOT_DIR="$BOOT_DIR/firmware"
+fi
+
+if [ -f $BOOT_DIR/candle_ssh.txt ] || [ -f $BOOT_DIR/candle_ssh_keep.txt ] || [ -f $BOOT_DIR/firmware/candle_ssh.txt ] || [ -f $BOOT_DIR/firmware/candle_ssh_keep.txt ] || [ ! -f $BOOT_DIR/firmware/candle_first_run_complete.txt ]; then
   systemctl start ssh.service
 fi
